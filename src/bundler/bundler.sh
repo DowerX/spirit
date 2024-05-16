7d80290944568c4d3994e6c236ac540b5676c4f8
BENEDEK <lacbenedek@gmail.com>
2024-05-16 19:03:26 +0200

    better bundler

--- src/bundler/bundler.sh
+++ src/bundler/bundler.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-MAPPING="build/bundler/mapping.h"
-HEADERS="build/bundler"
+# set -x
+
+BUNDLER="build/bundler"
+MAPPING="$BUNDLER/mapping.h"
+ASSETS_DIR="$BUNDLER/tmp/assets"
 
 cd "$(dirname "$0")"
 
 cd ../..
-mkdir -p build/bundler/assets
+mkdir -p "$ASSETS_DIR"
 
 ASSETS=""
 for x in "obj" "png" "vert" "frag"; do
-  ASSETS="$(find . -type f -name "*.$x")
+  ASSETS="$(find 'assets' -type f -name "*.$x")
 $ASSETS"
 done
 
-echo -e "#pragma once\n" > $HEADERS/assets/assets.h
+echo -e "#pragma once\n" > "$ASSETS_DIR/assets.h"
 
 echo -e "#pragma once
 
@@ -23,19 +26,31 @@ echo -e "#pragma once
 #include <assets/assets.h>
 
 namespace Bundler {
-static std::map<std::string, Asset> assets = {" > $MAPPING
+static std::map<std::string, Asset> assets = {" > "$MAPPING"
 
-for x in $ASSETS; do
-  file="${x:2}"
-  
-  header="${file%%.*}.c"
-  mkdir -p "$HEADERS/$(dirname $header)"
-  xxd -i "$file" > "$HEADERS/$header"
+for file in $ASSETS; do
+  name="${file%%.*}.o"
 
+  mkdir -p "$BUNDLER/tmp/$(dirname "$name")"
+
+  if [ -f "$BUNDLER/$name" ] && [ "$BUNDLER/$name" -nt "$file" ]; then
+    mv "$BUNDLER/$name" "$BUNDLER/tmp/$name"
+    var_name="$(echo "$file" | tr '/\-.' '_')"
+    echo "extern unsigned char $var_name[]; extern unsigned int ${var_name}_len;" >> "$ASSETS_DIR/assets.h"
+    echo "ASSET(\"$file\", $var_name)", >> "$MAPPING"
+
+    continue
+  fi
+
+  xxd -i "$file" | clang -x c -c -o "$BUNDLER/tmp/$name" -
 
   var_name="$(echo "$file" | tr '/\-.' '_')"
-  echo "extern unsigned char $var_name[]; extern unsigned int ${var_name}_len;" >> $HEADERS/assets/assets.h
-  echo "ASSET(\"$file\", $var_name)", >> $MAPPING
+  echo "extern unsigned char $var_name[]; extern unsigned int ${var_name}_len;" >> "$ASSETS_DIR/assets.h"
+  echo "ASSET(\"$file\", $var_name)", >> "$MAPPING"
 done
 
-echo "};}" >> $MAPPING
+echo "};}" >> "$MAPPING"
+
+rm -rf "$BUNDLER/assets"
+mv "$ASSETS_DIR" "$BUNDLER/assets"
+rm -rf "$BUNDLER/tmp"
